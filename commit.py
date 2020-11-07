9a3183de0afc57996ec8c98f4cd4aba4be157ab6
Tyler Blazey <blazey@wustl.edu>
2020-11-07 16:45:58 -0600

    Update commit.py

--- commit.py
+++ commit.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python
 
 #Load libraries
-import numpy as np
-import pandas as pd
-from datetime import datetime, timedelta
+from datetime import datetime
 from git import Repo
 
-
 #Commit
-repo_dir = r'C:\Users\polarice\Documents\Github\wustl-spinlab-status\'
+repo_dir = r'C:\Users\polarice\Documents\Github\wustl-spinlab-status'
 repo_time = datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
 repo = Repo(repo_dir)
 repo.index.add('temp_data.csv')
