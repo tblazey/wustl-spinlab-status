a478b21d5e1e887f4f32ba806bf1de49542c2f4e
Tyler Blazey <blazey@wustl.edu>
2020-11-07 17:33:30 -0600

    Update batch script

--- run_shot.bat
+++ run_shot.bat
@@ -1,3 +1,5 @@
 call C:\Users\polarice\Anaconda3\Scripts\activate.bat C:\Users\polarice\Anaconda3
 call pythonw "C:\Users\polarice\Documents\Github\wustl-spinlab-status\screenshot.py"
-timeout 1
+timeout 5
+call pythonw "C:\Users\polarice\Documents\Github\wustl-spinlab-status\commit.py"
+timeout 5
